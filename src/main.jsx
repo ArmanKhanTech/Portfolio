f14c355b4c43966a35b502df801cdb911be635f3
arman <ak2341776@gmail.com>
2023-12-13 22:26:33 +0530

    20th Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+
+import './index.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
