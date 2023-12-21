b049a652db1752f7532aa7ec5c90460f65715656
arman <ak2341776@gmail.com>
2023-12-21 14:30:59 +0530

    Fix emailjs public key import

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           to_email: "ak2341776@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
       )
       .then(
         () => {
