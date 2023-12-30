acb58066ca938118d395bcaf7490d6bb6b9b52e6
arman <ak2341776@gmail.com>
2023-12-30 12:51:44 +0530

    Update CTA section styling

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <section className='cta'>
+    <section className='cta  rounded-xl bg-blur p-3'>
       <p className='cta-text'>
         Have a project in mind? <br className='sm:block hidden' />
         Let’s build something together!
